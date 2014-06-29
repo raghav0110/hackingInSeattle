1e390aecfe6abc19ee77ce950789bec901f01914
Drew Ruberson <aruberso@purdue.edu>
2014-06-28 21:19:51 -0700

    dog test

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 var http = require('http');
+
+http.createServer(function(request, response) {
+	response.writeHead(200);
+	response.write("Dog is running.\n");
+	setTimeout(function() {
+		response.write("Dog is done.\n");
+		response.end();
+	}, 5000);
+}).listen(8080);
+console.log('listening on port 8080...');
